2ee7bb18330c971d7647294c8e0a0d0a8a625b3c
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2022-06-16 15:37:41 -0500

    Terminando footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container-fluid bg-dark p-4 d-flex flex-column justify-content-center align-items-center">
+        <p class="text-center text-white h3">Tadeo Alvarez &copy; Todos los derechos reservados</p>
+        <p class="text-center text-white">Todas las recetas aqui vistas fueron tomadas de: <a class="text-white" href="https://www.cocinafacil.com.mx/">Cocina-Facil.com.mx</a></p>
+    </div>
+</template>
+
+<script>
+export default { 
+    name: "TheFooter"
+}
+</script>
--- src/views/Home-Page.vue
+++ src/views/Home-Page.vue
@@ -2,6 +2,7 @@
   <Hero-section/>
   <The-secciones/>
   <Recetas-destacadas-section/>
+  <The-footer/>
 </template>
 
 <script>
@@ -9,8 +10,9 @@ import HeroSection from '@/components/TheHero.vue';
 import RecetasDestacadasSection from '@/components/TheDestacados.vue';
 import JSONData from '@/db/db.json';
 import TheSecciones from '@/components/TheSecciones.vue';
+import TheFooter from '@/components/TheFooter.vue';
 export default {
-    components: {HeroSection, RecetasDestacadasSection, TheSecciones},
+    components: {HeroSection, RecetasDestacadasSection, TheSecciones, TheFooter},
     data() {
         return {
             recetas: JSONData.recetas.postres,
