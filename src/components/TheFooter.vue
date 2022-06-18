00ebdc7d7d19da276fb9449b38217d057963df00
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2022-06-17 20:05:54 -0500

    Agregando Favicon, y error 404 en rutas dinamicas

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid bg-dark p-4 d-flex flex-column justify-content-center align-items-center">
         <p class="text-center text-white h3">Tadeo Alvarez &copy; Todos los derechos reservados</p>
         <p class="text-center text-white">Todas las recetas aqui vistas fueron tomadas de: <a class="text-white" href="https://www.cocinafacil.com.mx/">Cocina-Facil.com.mx</a></p>
-        <a href="https://www.flaticon.com/free-icons/page-not-found" title="page not found icons">Page not found icon creado por smashingstocks - Flaticon</a>
+        <a class="text-center text-white" href="https://www.flaticon.com/free-icons/page-not-found" title="page not found icons">Page not found icon creado por smashingstocks - Flaticon</a>
     </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home-Page.vue';
+import JSONData from '@/db/db.json';
 
 const routes = [
   {
@@ -15,12 +16,38 @@ const routes = [
   {
     path: '/recetas/:tipo',
     name: 'recetas',
-    component: () => import('@/views/ViewsTemplate.vue')
+    component: () => import('@/views/ViewsTemplate.vue'),
+    beforeEnter(to) {
+      const ArregloRecetas = [];
+      JSONData.recetas.map(tipo => {
+         tipo.recetas.map(receta => ArregloRecetas.push(receta))
+      });
+
+      const receta = ArregloRecetas.find(receta => receta.nombre === to.params.nombre);
+      if(receta == undefined) {
+        return {
+          name: "NotFound"
+        }
+      }
+    },
   },
   {
     path: '/recetas/:grupo/:nombre',
     name: 'receta.grupo',
-    component: () => import('@/views/RecetaShow.vue')
+    component: () => import('@/views/RecetaShow.vue'),
+    beforeEnter(to) {
+      const ArregloRecetas = [];
+      JSONData.recetas.map(tipo => {
+         tipo.recetas.map(receta => ArregloRecetas.push(receta))
+      });
+
+      const receta = ArregloRecetas.find(receta => receta.nombre === to.params.nombre);
+      if(receta == undefined) {
+        return {
+          name: "NotFound"
+        }
+      }
+    },
   },
   {
     path: "/:pathMatch(.*)*",
